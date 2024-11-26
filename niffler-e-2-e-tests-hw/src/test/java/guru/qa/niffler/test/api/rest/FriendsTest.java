4135960a509abbfb87cd8e91fff775df981710dd
Arrnel <arrnel@vk.com>
2024-11-26 19:07:56 +0300

    Gateway friend and invitations tests

--- niffler-e-2-e-tests-hw/src/test/java/guru/qa/niffler/test/api/rest/FriendsTest.java
+++ niffler-e-2-e-tests-hw/src/test/java/guru/qa/niffler/test/api/rest/FriendsTest.java
@@ -1,5 +1,7 @@
 package guru.qa.niffler.test.api.rest;
 
+import guru.qa.niffler.api.core.ThreadSafeCookieStore;
+import guru.qa.niffler.api.core.store.AuthStore;
 import guru.qa.niffler.api.gateway.v1.UserdataV1ApiClientRetrofit;
 import guru.qa.niffler.enums.FriendState;
 import guru.qa.niffler.jupiter.annotation.ApiLogin;
@@ -11,10 +13,12 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @RestTest
 class FriendsTest {
 
-    private final UserdataV1ApiClientRetrofit userdataV1ApiClient = new UserdataV1ApiClientRetrofit();
+    private final UserdataV1ApiClientRetrofit userdataClient = new UserdataV1ApiClientRetrofit();
 
     @Test
     void allFriendsAndIncomeInvitationsShouldBeReturnedFroUser(
@@ -28,7 +32,7 @@ class FriendsTest {
         final List<UserJson> expectedFriends = user.getTestData().getFriends();
         final List<UserJson> expectedInvitations = user.getTestData().getIncomeInvitations();
 
-        final List<UserJson> result = userdataV1ApiClient.allFriends(
+        final List<UserJson> result = userdataClient.allFriends(
                 user.getTestData().getToken(),
                 null
         );
@@ -64,6 +68,161 @@ class FriendsTest {
                 expectedFriends.getLast().getUsername(),
                 secondUserFromRequest.getUsername()
         );
+    }
+
+    @Test
+    void shouldExists2FriendRecordsAfterAcceptInvitationTest(
+
+            @CreateNewUser
+            @ApiLogin
+            UserJson user,
+
+            @CreateNewUser
+            @ApiLogin
+            UserJson target
+
+    ) {
+
+        // Data
+        var userToken = user.getTestData().getToken();
+        var targetToken = target.getTestData().getToken();
+
+        // Steps
+        var sendInvitation = userdataClient.sendInvitation(
+                targetToken,
+                user.getUsername());
+
+        var acceptInvitation = userdataClient.acceptInvitation(
+                userToken,
+                target.getUsername());
+
+        var userFriends = userdataClient.allFriends(
+                userToken,
+                target.getUsername());
+
+        var targetFriends = userdataClient.allFriends(
+                targetToken,
+                user.getUsername());
+
+        assertAll("Both users have each other as friends after accept invitation",
+
+                () -> assertEquals(FriendState.INVITE_SENT, sendInvitation.getFriendState()),
+
+                () -> assertEquals(FriendState.FRIEND, acceptInvitation.getFriendState()),
+
+                () -> assertTrue(userFriends.stream()
+                        .anyMatch(tu ->
+                                tu.getUsername().equals(target.getUsername()) &&
+                                        tu.getFriendState() == FriendState.FRIEND
+                        )),
+
+                () -> assertTrue(targetFriends.stream()
+                        .anyMatch(u ->
+                                u.getUsername().equals(user.getUsername()) &&
+                                        u.getFriendState() == FriendState.FRIEND))
+        );
+
+    }
+
+    @Test
+    void shouldNotExistFriendRecordsAfterDeclineInvitationTest(
+
+            @CreateNewUser
+            @ApiLogin
+            UserJson user,
+
+            @CreateNewUser
+            @ApiLogin
+            UserJson target
+
+    ) {
+
+        // Data
+        var userToken = user.getTestData().getToken();
+        var targetToken = target.getTestData().getToken();
+
+        // Steps
+        var sendInvitation = userdataClient.sendInvitation(
+                targetToken,
+                user.getUsername());
+
+        var declineInvitation = userdataClient.declineInvitation(
+                userToken,
+                target.getUsername());
+
+        var userFriends = userdataClient.allFriends(
+                userToken,
+                target.getUsername());
+
+        var targetFriends = userdataClient.allFriends(
+                targetToken,
+                user.getUsername());
+
+        assertAll("Both users should not have each other as friends after decline invitation",
+
+                () -> assertEquals(FriendState.INVITE_SENT, sendInvitation.getFriendState()),
+
+                () -> assertNull(declineInvitation.getFriendState()),
+
+                () -> assertTrue(userFriends.stream()
+                        .noneMatch(u ->
+                                u.getUsername().equals(target.getUsername()))),
+                () -> assertTrue(targetFriends.stream()
+                        .noneMatch(u ->
+                                u.getUsername().equals(user.getUsername())))
+        );
+
+    }
+
+    @Test
+    void shouldNotExistFriendRecordsAfterRemoveFriendTest(
+
+            @CreateNewUser(
+                    friends = 1
+            )
+            @ApiLogin
+            UserJson user,
+
+            @CreateNewUser
+            @ApiLogin
+            UserJson target
+
+    ) {
+
+        // Data
+        var userToken = user.getTestData().getToken();
+        var targetToken = target.getTestData().getToken();
+
+        // Steps
+        userdataClient.sendInvitation(
+                targetToken,
+                user.getUsername());
+
+        userdataClient.acceptInvitation(
+                userToken,
+                target.getUsername());
+
+        userdataClient.removeFriend(
+                userToken,
+                target.getUsername());
+
+        var userFriends = userdataClient.allFriends(
+                userToken,
+                target.getUsername());
+
+        var targetFriends = userdataClient.allFriends(
+                targetToken,
+                user.getUsername());
+
+        assertAll("Both users should not have each other as friends after friend removed",
+                () -> assertTrue(userFriends.stream()
+                        .noneMatch(tu ->
+                                tu.getUsername().equals(target.getUsername()))),
+                () -> assertTrue(targetFriends.stream()
+                        .noneMatch(u ->
+                                u.getUsername().equals(user.getUsername())))
+        );
+
     }
 
 }
